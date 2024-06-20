7ffe94178f28efa534a041352b22ff18e7af36df
sk1lek <kontakt.sk1lek@gmail.com>
2024-06-20 08:26:27 +0200

    Zmiany

--- docs/earthsmp/poczatki/coto.md
+++ docs/earthsmp/poczatki/coto.md
@@ -3,3 +3,9 @@ sidebar_position: 1
 ---
 
 # Co to EarthSMP?
+
+Tryb EarthSMP u nas to jest po prostu Survival tylko że na mapie świata, z róźnymi dodakowymi funkcjami takimi jak np. Farmerzy, Dom Aukcyjny, Ulepszalne Spawnery, Pojazdy i wiele więcej...
+
+Tak jak pisaliśmy rozgrywka odbywa się na **Prawdziwej Mapie Świata** aby zobaczyć gdzie się jest wystarczy wejść na stronę https://mapa.minecube.pl
+
+[**Jesteś nowy? Nie wiesz jak zacząć? Zajrzyj tutaj!**](/docs/earthsmp/poczatki/jakzaczac)
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -60,7 +60,7 @@ const config: Config = {
           type: "docSidebar",
           sidebarId: "tutorialSidebar",
           position: "left",
-          label: "Tutorial",
+          label: "Wiki",
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
@@ -82,7 +82,7 @@ const config: Config = {
           title: "Docs",
           items: [
             {
-              label: "Tutorial",
+              label: "Wiki",
               to: "/docs/intro",
             },
           ],
@@ -95,21 +95,8 @@ const config: Config = {
               href: "https://dc.minecube.pl",
             },
             {
-              label: "Twitter",
-              href: "https://twitter.com/docusaurus",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "Blog",
-              to: "/blog",
-            },
-            {
-              label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              label: "TikTok",
+              href: "https://www.tiktok.com/@rubylandpl",
             },
           ],
         },
